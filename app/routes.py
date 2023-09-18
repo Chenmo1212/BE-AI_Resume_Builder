c96fcea57b77abe6cf7f33146c607e47e3066354
chenmo <chenmo1212@qq.com>
2023-09-18 13:19:14 +0100

    fix: done job api & job model & job test api

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from app import app, mongo
-from app.models import Message, ResumeManager
+from app.models import Message, ResumeManager, JobManager
 from datetime import datetime
 import requests, json
 from bson import ObjectId
@@ -19,8 +19,8 @@ def insert_resume():
     try:
         data = request.get_json()
         manager = ResumeManager()
-        resume_id = manager.create_resume(data)
-        return jsonify({"message": "Resume created successfully", "inserted_id": resume_id}), 201
+        resume_id = manager.create(data)
+        return jsonify({"message": "Resume created successfully", "inserted_id": str(resume_id)}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
@@ -28,7 +28,7 @@ def insert_resume():
 @app.route('/resume/<resume_id>', methods=['GET'])
 def get_resume(resume_id):
     manager = ResumeManager()
-    resume = manager.get_resume(resume_id)
+    resume = manager.get(resume_id)
     if resume:
         return jsonify(resume)
     return jsonify({"error": "Resume not found"}), 404
@@ -39,7 +39,7 @@ def update_resume(resume_id):
     try:
         data = request.get_json()
         manager = ResumeManager()
-        modified_count = manager.update_resume(resume_id, data)
+        modified_count = manager.update(resume_id, data)
         if modified_count > 0:
             return jsonify({"message": "Resume updated successfully"}), 200
         return jsonify({"error": "Resume not found or already deleted"}), 404
@@ -50,7 +50,7 @@ def update_resume(resume_id):
 @app.route('/resume/<resume_id>', methods=['DELETE'])
 def delete_resume(resume_id):
     manager = ResumeManager()
-    modified_count = manager.delete_resume(resume_id)
+    modified_count = manager.delete(resume_id)
     if modified_count > 0:
         return jsonify({"message": "Resume deleted successfully"}), 200
     return jsonify({"error": "Resume not found or already deleted"}), 404
@@ -59,19 +59,58 @@ def delete_resume(resume_id):
 @app.route('/resumes', methods=['GET'])
 def list_resumes():
     manager = ResumeManager()
-    resumes = manager.list_resumes()
+    resumes = manager.list()
     return jsonify(resumes)
 
 
-@app.route('/resume', methods=['GET'])
-def get_resumes():
+# Job APIs
+@app.route('/job', methods=['POST'])
+def insert_job():
     try:
         data = request.get_json()
-        manager = ResumeManager()
-        resume_id = manager.create_resume(data)
-        return jsonify({"message": "Data inserted successfully", "inserted_id": str(resume_id)})
+        manager = JobManager()
+        job_id = manager.create(data)
+        return jsonify({"message": "Job created successfully", "inserted_id": str(job_id)}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+
+@app.route('/job/<job_id>', methods=['GET'])
+def get_job(job_id):
+    manager = JobManager()
+    job = manager.get(job_id)
+    if job:
+        return jsonify(job)
+    return jsonify({"error": "Job not found"}), 404
+
+
+@app.route('/job/<job_id>', methods=['PUT'])
+def update_job(job_id):
+    try:
+        data = request.get_json()
+        manager = JobManager()
+        modified_count = manager.update(job_id, data)
+        if modified_count > 0:
+            return jsonify({"message": "Job updated successfully"}), 200
+        return jsonify({"error": "Job not found or already deleted"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+
+@app.route('/job/<job_id>', methods=['DELETE'])
+def delete_job(job_id):
+    manager = JobManager()
+    modified_count = manager.delete(job_id)
+    if modified_count > 0:
+        return jsonify({"message": "Job deleted successfully"}), 200
+    return jsonify({"error": "Job not found or already deleted"}), 404
+
+
+@app.route('/jobs', methods=['GET'])
+def list_jobs():
+    manager = JobManager()
+    jobs = manager.list()
+    return jsonify(jobs)
 
 
 # ============ User Api ============
