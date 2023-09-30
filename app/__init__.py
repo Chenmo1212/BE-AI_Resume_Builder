d327acc97fb750618b409ba1d5333c2fea88480f
chenmo <chenmo1212@qq.com>
2023-10-01 00:15:10 +0100

    feat: add CORS to prevent Domain Cross

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the appropriate configuration based on the environment
 # if app.env == 'production':
